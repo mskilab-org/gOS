4297fbc84f47ab4074e55082118740750d32f48a
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2024-06-11 23:06:43 +0200

    removed unnecessary console.log statements

--- src/helpers/utility.js
+++ src/helpers/utility.js
@@ -658,9 +658,7 @@ export function getSignatureMetrics(
       plot.id = d;
       plot.type = "histogram";
       plot.scaleX = "linear";
-      console.log(d, populations[d]);
       plot.allData = populations[d].map((e) => +e.value);
-      console.log(d, populations[d]);
       plot.data = populations[d]
         .filter((e) =>
           tumour_type ? !e.tumor_type || e.tumor_type === tumour_type : true
