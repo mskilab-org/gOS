56be344524b8e84f46d7061c9d700a52a07e589d
Shihab Dider <shihabdider@gmail.com>
2024-08-21 15:13:31 -0400

    changed the tooltip attributes
    Previous tooltip attributes were taking up too much space and could be condensed into fewer entries.
    The annotations attribute has been split into separate entries.
    Adds ref/alt/total count attribute to previous vaf field
    Adds normal ref/alt/total counts
    Adds guards to prevent falsy values from showing up in the tooltip
    Adds g variant (requires implementing in snv multiplicity, implemented in skilift)

--- src/components/genomePlot/interval.js
+++ src/components/genomePlot/interval.js
@@ -53,22 +53,54 @@ class Interval {
 
   get tooltipContent() {
     let attributes = [
-      { label: "iid", value: this.iid },
-      { label: "annotation", value: this.annotation },
-      { label: "Chromosome", value: this.chromosome },
-      { label: "Y", value: this.y },
-      { label: "Start Point", value: d3.format(",")(this.startPoint) },
-      { label: "End Point", value: d3.format(",")(this.endPoint) },
-      {
-        label: "Length",
-        value: d3.format(",")(this.endPoint - this.startPoint),
-      },
+      { label: "Locus", value: this.location},
+      { label: "CN", value: this.y },
     ];
+    if (this.annotation) {
+      const annotations = this.annotation.split(";").map((item) => {
+        if (!item.includes(":")) {
+          return null;
+        }
+        const [key, value] = item.split(":");
+        return { key: key.trim(), value: value.trim() };
+      });
+      const gene = annotations.find((item) => item.key === "Gene")?.value;
+      const filter = annotations.find((item) => item.key === "Filter")?.value;
+      const variant = annotations.find((item) => item.key === "Variant")?.value;
+      const protein_variant = annotations.find((item) => item.key === "Protein_variant")?.value;
+      const gene_variant = annotations.find((item) => item.key === "Genomic_variant")?.value;
+      const alt_count = annotations.find((item) => item.key === "Alt_count")?.value;
+      const ref_count = annotations.find((item) => item.key === "Ref_count")?.value;
+      const total_count = ref_count && alt_count ? parseInt(ref_count) + parseInt(alt_count) : null;
+      const vaf = Number(annotations.find((item) => item.key === "VAF")?.value).toFixed(3);
+      const normal_alt_count = annotations.find((item) => item.key === "Normal_alt_count")?.value;
+      const normal_ref_count = annotations.find((item) => item.key === "Normal_ref_count")?.value;
+      const normal_total_count = normal_ref_count && normal_alt_count ? parseInt(normal_ref_count) + parseInt(normal_alt_count) : null;
+
+      if (gene) {
+        attributes.push({ label: "Gene", value: gene });
+      }
+      if (filter) {
+        attributes.push({ label: "Filter", value: filter });
+      }
+      if (variant || protein_variant || gene_variant) {
+        attributes.push({ label: "Variant", value: `${variant}, ${protein_variant}, ${gene_variant}` });
+      }
+      if (ref_count && alt_count && total_count && vaf) {
+        attributes.push({ label: "Tumor ALT | REF | TOTAL | VAF", value: `${alt_count} | ${ref_count} | ${total_count} | ${vaf}` });
+      }
+      if (normal_alt_count && normal_ref_count && normal_total_count) {
+        attributes.push({ label: "Normal ALT | REF | TOTAL", value: `${normal_alt_count} | ${normal_ref_count} | ${normal_total_count}` });
+      }
+    }
     if (this.strand) {
       attributes.push({ label: "Strand", value: this.strand });
     }
     if (this.sequence) {
       attributes.push({ label: "Sequence", value: this.sequence });
+    }
+    if (this.intervalLength > 1) {
+      attributes.push({ label: "Length", value: d3.format(',')(this.intervalLength) });
     }
     Object.keys(this.metadata).forEach((key) => {
       attributes.push({ label: humanize(key), value: this.metadata[key] });
