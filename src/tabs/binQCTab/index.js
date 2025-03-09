36f765889249d71415cf5a5e36420593c80af7dc
C.Xanthopoulakis <c.xanthopoulakis@gmail.com>
2025-03-09 21:20:33 +0200

    added the png images for purity/ploidy grid search

--- src/tabs/binQCTab/index.js
+++ src/tabs/binQCTab/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 import { connect } from "react-redux";
-import { Row, Col } from "antd";
+import { Row, Col, Image } from "antd";
 import Wrapper from "./index.style";
 import BinPlotPanel from "../../components/binPlotPanel";
 import SnvplicityPlotPanel from "../../components/snvplicityPlotPanel";
 
 class BinQcTab extends Component {
   render() {
+    const { dataset, id } = this.props;
     return (
       <Wrapper>
         <Row
@@ -26,6 +27,19 @@ class BinQcTab extends Component {
             <SnvplicityPlotPanel />
           </Col>
         </Row>
+        <Row
+          className="ant-panel-container ant-home-plot-container"
+          gutter={16}
+        >
+          <Col className="gutter-row" span={12}>
+            <Image src={`${dataset.dataPath}${id}/purple_sunrise_pp.png`} />
+          </Col>
+          <Col className="gutter-row" span={12}>
+            <Image
+              src={`${dataset.dataPath}${id}/purple_sunrise_beta_gamma.png`}
+            />
+          </Col>
+        </Row>
       </Wrapper>
     );
   }
@@ -36,6 +50,7 @@ const mapDispatchToProps = (dispatch) => ({});
 const mapStateToProps = (state) => ({
   metadata: state.CaseReport.metadata,
   dataset: state.Settings.dataset,
+  id: state.CaseReport.id,
 });
 export default connect(
   mapStateToProps,
