36f5725fef680357589a6b09d3408c89a58ba6bc
Shihab Dider <shihabdider@gmail.com>
2025-06-16 10:26:03 -0400

    chore: clean up after merge

--- src/containers/listView/index.js
+++ src/containers/listView/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 import { connect } from "react-redux";
-import GoslingChat from '../../components/goslingChat';
 import {
   Row,
   Col,
