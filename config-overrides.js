558286fcfb51676bfc42921577b154aa8ba5916a
kevinmhadi <kevinmhadi.gmail.com>
2026-05-05 15:54:21 -0400

    Expose shared directory when building from npm start. Also handling 404 errors gracefully for missing Genome View tracks when running via local build (npm start).

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,35 @@
+const path = require("path");
+
 module.exports = function override(webpackConfig) {
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
-    type: "javascript/auto"
+    type: "javascript/auto",
   });
 
   return webpackConfig;
-}
+};
+
+module.exports.devServer = function overrideDevServer(configFunction) {
+  return function (proxy, allowedHost) {
+    const config = configFunction(proxy, allowedHost);
+
+    const existingStatic = config.static;
+    const staticDirs = Array.isArray(existingStatic)
+      ? existingStatic
+      : existingStatic
+        ? [existingStatic]
+        : [];
+
+    config.static = [
+      ...staticDirs,
+      {
+        directory: path.resolve(__dirname, "shared"),
+        publicPath: "/",
+        watch: false,
+      },
+    ];
+
+    return config;
+  };
+};
